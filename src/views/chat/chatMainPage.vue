87eb153de48210861f5299f44c519e4aea844504
Alan <1563085328@qq.com>
2024-04-21 16:40:32 +0800

    add chat page

--- src/views/chat/chatMainPage.vue
+++ src/views/chat/chatMainPage.vue
@@ -1,11 +1,39 @@
 <script setup>
+import dockComp from './child/dockComp.vue';
 </script>
 
 <template>
     <div>
-        这是聊天页面
+        <a-split 
+            class="containers"
+            default-size="350"
+            min="250"
+            max="500"
+        >
+            <template #first>
+                <dockComp></dockComp>
+            </template>
+            <template #second>
+                <div>
+                    <a-split direction="vertical" :style="{height: '100%'}" default-size="600" max="600" min="200">
+                        <template #first>
+
+                        </template>
+                        <template #second>
+
+                        </template>
+                    </a-split>
+                </div>
+            </template>
+        </a-split>
     </div>
 </template>
 
 <style lang="scss" scoped>
+.containers{
+    height: calc(100vh - 1px);
+    width: calc(100vw - 1px);
+    min-width: '500px';
+    // border: '1px solid var(--color-border)';
+}
 </style>
