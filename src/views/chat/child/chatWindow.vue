643a48900cf7176fcd69dbe497eb0470a1dfc201
Alan <1563085328@qq.com>
2024-04-27 11:35:01 +0800

    test

--- src/views/chat/child/chatWindow.vue
+++ src/views/chat/child/chatWindow.vue
@@ -6,7 +6,7 @@ const test = ref("1");
 const roomType = ref(1)
 const chatList = ref()
 const myId = "111"
-
+const test1 = ref("")
 onMounted(() => {
     console.log(test.value);
     chatList.value = [
